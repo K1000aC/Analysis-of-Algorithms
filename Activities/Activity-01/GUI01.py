cbd37aed0b7fa6a56ef2403bb4cf4042826bd916
Camila Cruz <camilasuarez002@gmail.com>
2026-08-27 16:35:30 -0600

    Add GUI application and commit instructions; remove unnecessary .gitkeep files

---
+++ Activities/Activity-01/GUI01.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+
+x = [3, 4, 5, 6, 7]
+y = [10, 12, 14, 16, 18]
+
+plt.plot(x, y)
+plt.scatter(x, y, color='purple') #Gráfica de puntos
+#plt.bar(x, y, color='lightpink') #Gráfica de barras
+plt.title("Mi primera Gráfica")
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
+plt.show()
+
+def saludar():
+    nombre = entrada.get().strip()
+    if not nombre:
+        nombre = "Camila"
+    lbl.config(text=f"Hola Compa, {nombre}!!")
+
+root=tk.Tk()
+root.title("Saludador - My First GUI")
+root.geometry("500x600")
+lbl = tk.Label(root, text="Eh compa escribe tu nombre y presiona el botón",background="lightpink", foreground="white", font=("Arial Rounded MT Bold", 16))
+lbl.pack(pady=20)
+entrada = tk.Entry(root, foreground ="pink", font=("Arial Rounded MT Bold", 12))
+entrada.pack(pady=10)
+bot = tk.Button(root, text="Saludar", command=saludar, font=("Arial Rounded MT Bold", 12))
+bot.pack(pady=10)
+
+root.mainloop()
+
---
+++ Activities/Activity-01/Pasos para commit.md
@@ -0,0 +1,17 @@
+# pasos para hacer commit en su github
+
+Asegurarse que estan en la carpeta a respaldar en github
+
+- git init
+- git add .
+- git branch -M main
+- git remote add origin <su link de github que termina en .git> ejemplo: https://github.com/ernestolar/A_algo.git
+- git config user
+- git commit -m "primer commit"
+- git push ---set-upstream origin main
+
+# Para futuros cambios solo:
+
+- git add .
+- git commit -m "su mensaje de cambio"
+-
